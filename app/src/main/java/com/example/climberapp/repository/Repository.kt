b086fd822aaef3d699877156a0ca2971e4182e7f
ArtiJha <erartivats49@gmail.com>
2023-06-15 17:50:24 +0530

    logout functionality

--- app/src/main/java/com/example/climberapp/repository/Repository.kt
+++ app/src/main/java/com/example/climberapp/repository/Repository.kt
@@ -2,7 +2,7 @@ package com.example.climberapp.repository
 
 import com.example.climberapp.retrofit.RetrofitService
 
-class DeviceRepository constructor(private val retrofitService: RetrofitService) {
+class Repository constructor(private val retrofitService: RetrofitService) {
 
     fun getAllDevices() = retrofitService.getAllDevices()
 }
--- app/src/main/java/com/example/climberapp/ui/devices/DevicesFragment.kt
+++ app/src/main/java/com/example/climberapp/ui/devices/DevicesFragment.kt
@@ -1,20 +1,32 @@
 package com.example.climberapp.ui.devices
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import com.example.climberapp.R
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.climberapp.adapters.DeviceAdapter
 import com.example.climberapp.databinding.FragmentDevicesBinding
-import com.example.climberapp.repository.DeviceRepository
+import com.example.climberapp.repository.Repository
 import com.example.climberapp.retrofit.RetrofitService
-import com.example.climberapp.viewHolder.DeviceViewModel
 import com.example.climberapp.viewHolder.MyViewModelFactory
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asFlow
+import androidx.lifecycle.whenCreated
+import com.example.climberapp.models.DeviceListModel
 
 class DevicesFragment : Fragment() {
     private val TAG = "MainActivity"
@@ -31,36 +43,28 @@ class DevicesFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-/*
-*  setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setHasOptionsMenu(true)
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Handle the back button event
+            }
+        }
+        requireActivity().getOnBackPressedDispatcher().addCallback(this, onBackPressedCallback)
+        super.onCreate(savedInstanceState)
+    }
 
-        viewModel = ViewModelProvider(this, MyViewModelFactory(MainRepository(retrofitService))).get(MainViewModel::class.java)
 
-        binding.recyclerview.adapter = adapter
-
-        viewModel.movieList.observe(this, Observer {
-            Log.d(TAG, "onCreate: $it")
-            adapter.setMovieList(it)
-        })
-
-        viewModel.errorMessage.observe(this, Observer {
-
-        })
-        viewModel.getAllMovies()*/
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
        // val galleryViewModel = ViewModelProvider(this).get(DevicesViewModel::class.java)
-
-
         _binding = FragmentDevicesBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-    viewModel = ViewModelProvider(this, MyViewModelFactory(DeviceRepository(retrofitService))).get(DeviceViewModel::class.java)
+    viewModel = ViewModelProvider(this, MyViewModelFactory(Repository(retrofitService),requireContext())).get(DeviceViewModel::class.java)
     binding.recyclerview.adapter = adapter
 
     viewModel.deviceList.observe(viewLifecycleOwner, Observer {
@@ -75,13 +79,55 @@ class DevicesFragment : Fragment() {
     }
     viewModel.getAllDevices()
 
-
 //        val textView: TextView = binding.textDevices
 //        galleryViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
         return root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_menu,menu)
+        val searchItem = menu.findItem(R.id.actionSearch)
+        val searchView = searchItem.actionView as SearchView
+        //searching(searchView as SearchView)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query.isNotBlank()||query.isNotEmpty()) {
+                    adapter.filter.filter(query)
+                    adapter.notifyDataSetChanged()
+                }
+                return false
+            }
+
+            @SuppressLint("NotifyDataSetChanged")
+            override fun onQueryTextChange(newText: String): Boolean {
+                if (newText.isNotBlank()||newText.isNotEmpty()){
+                    adapter.filter.filter(newText)
+                    adapter.notifyDataSetChanged()
+                }
+                return false
+            }
+        })
+
+        searchView.setOnQueryTextFocusChangeListener { _ , hasFocus ->
+            if (hasFocus) {
+                Log.d("focused","sear")
+            } else {
+                    // searchView not expanded
+                    viewModel.deviceList.observe(viewLifecycleOwner, Observer {
+                        Log.d(TAG, "onCreate: $it")
+                        adapter.setDeviceList(it)
+                    })
+                Log.d("colapsed","close")
+            }
+        }
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
